b8041992eb06a2fab89ff072efe9fd2e8e745f07
sorosh.saloti <sorosh.saloti@pishtazan.onmicrosoft.com>
2022-03-23 01:59:03 +0300

    Fix struct issue

--- src/RocketsLand/InfoStruct/Coordinate.cs
+++ src/RocketsLand/InfoStruct/Coordinate.cs
@@ -18,58 +18,5 @@ public struct Coordinate
         var y = this.Y + n;
         return new Coordinate(x, y);
     }
-    public static bool operator ==(Coordinate? left, Coordinate? right)
-    {
-        if (left is null)
-        {
-            if (right is null)
-            {
-                return true;
-            }
-
-            // Only the left side is null.
-            return false;
-        }
-        // Equals handles case of null on right side.
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(Coordinate? left, Coordinate? right)
-    {
-        if (left is null)
-        {
-            if (right is null)
-            {
-                return true;
-            }
-
-            // Only the left side is null.
-            return false;
-        }
-        // Equals handles case of null on right side.
-        return !left.Equals(right);
-    }
-
-    public override bool Equals([NotNullWhen(true)] object? obj) => this.Equals(obj as Coordinate?);
-
-    public bool Equals(Coordinate? c)
-    {
-        if (c is null)
-        {
-            return false;
-        }
-
-        if (Object.ReferenceEquals(this, c))
-        {
-            return true;
-        }
-
-        if (this.GetType() != c.GetType())
-        {
-            return false;
-        }
-
-        return ( X == c?.X ) && ( Y == c?.Y );
-    }
-
+     
 }
