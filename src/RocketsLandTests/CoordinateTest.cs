180aa6d4bc88c7e4a2ff853cb90a6524c02b3c09
sorosh.saloti <sorosh.saloti@pishtazan.onmicrosoft.com>
2022-03-23 09:24:40 +0300

    code review

--- src/RocketsLandTests/CoordinateTest.cs
+++ src/RocketsLandTests/CoordinateTest.cs
@@ -12,4 +12,13 @@ public class CoordinateTest
         Coordinate coordinate = new Coordinate ( 10, 30 );
         coordinate.ToString().Should().Be("10,30");
     }
+    [Fact]
+    public void WhenCoordinateAdd()
+    {
+        Coordinate coordinate = new Coordinate(10, 30);
+        var new1=coordinate.Add(1);
+        new1.ToString().Should().Be("11,31");
+        var new2 =new1.Add(-1);
+        coordinate.Should().Be(new2);
+    }
 }
